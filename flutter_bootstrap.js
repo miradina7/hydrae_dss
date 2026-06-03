46f6f43ccfa59e6bae2bfe659ae8cef873798132
miradina7 <syahdina.asbb@gmail.com>
2026-06-03 16:10:58 +0800

    Fix box error and update icons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1817198737" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3600179317" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
